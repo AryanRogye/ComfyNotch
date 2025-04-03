9c8586428c588ef48c769443bc50192426ec488c
AryanRogye <aryan.rogye@gmail.com>
2025-04-03 10:04:05 -0500

    added a version check to make sure on macos version 14>

--- Sources/ComfyNotch/main.swift
+++ Sources/ComfyNotch/main.swift
@@ -1,5 +1,16 @@
 import AppKit
 
+let minimumSupportedVersion = OperatingSystemVersion(majorVersion: 14, minorVersion: 0, patchVersion: 0) // macOS 14 (Sonoma)
+
+if !ProcessInfo.processInfo.isOperatingSystemAtLeast(minimumSupportedVersion) {
+    let alert = NSAlert()
+    alert.messageText = "Unsupported macOS Version"
+    alert.informativeText = "ComfyNotch requires macOS 14 (Sonoma) or higher. Please update your system."
+    alert.alertStyle = .critical
+    alert.runModal()
+    exit(0)
+}
+
 let app = NSApplication.shared
 let delegate = AppDelegate()
 app.delegate = delegate
