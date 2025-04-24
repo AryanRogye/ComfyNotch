4355765b86548de46a479bf01848d6586f910883
AryanRogye <aryan.rogye@gmail.com>
2025-04-24 12:31:26 -0500

    made sure that the filetray shows the files that were saved to the folder, super tiny bug that videos or some images wont work, thats cuz the starting word is diff, i gotta find a way to get past that,maybe by just appending my own signature after it, for now

--- ComfyNotch/ComfyNotchView.swift
+++ ComfyNotch/ComfyNotchView.swift
@@ -211,16 +211,40 @@ struct ComfyNotchView: View {
         return 180
     }
     
+    private func getFilesFromStoredDirectory() -> [URL] {
+        let fileManager = FileManager.default
+        let folderURL = settings.fileTrayDefaultFolder
+        var matchedFiles: [URL] = []
+        /// we wanna return the ones that start with a "DroppedImage" name
+        /// This is the one that we added to that selected "Directory", if the user wants to remove
+        /// or change the name then it wont show anymore and thats ok, thats up
+        /// to them
+        
+        /// settings.fileTrayDefaultFolder is the folder to watch for
+        do {
+            let contents = try fileManager.contentsOfDirectory(at: folderURL, includingPropertiesForKeys: nil)
+            for url in contents {
+                if url.lastPathComponent.hasPrefix("DroppedImage") {
+                    matchedFiles.append(url)
+                }
+            }
+        } catch {
+            print("There Was A Error Getting Paths \(error.localizedDescription)")
+        }
+        return matchedFiles
+    }
+    
     @ViewBuilder
     private func renderFileTray() -> some View {
         VStack(spacing: 0) {
             if animationState.isExpanded {
-                if animationState.droppedFiles.isEmpty {
+                let droppedFiles = self.getFilesFromStoredDirectory()
+                if droppedFiles.isEmpty {
                     Text("No Files Yet")
                     Spacer()
                 } else {
                     ScrollView {
-                        ForEach(animationState.droppedFiles, id: \.self) { fileURL in
+                        ForEach(droppedFiles, id: \.self) { fileURL in
                             HStack {
                                 Text(fileURL.lastPathComponent)
                                     .foregroundColor(.white)
