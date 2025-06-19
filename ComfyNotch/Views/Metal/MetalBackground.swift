7ccb60b86a09c8474293248436808aa5bdb7cc14
AryanRogye <aryan.rogye@gmail.com>
2025-06-18 21:45:29 -0500

    fixed a mini issue

--- ComfyNotch/Views/Metal/MetalBackground.swift
+++ ComfyNotch/Views/Metal/MetalBackground.swift
@@ -145,7 +145,8 @@ struct MetalBackground: NSViewRepresentable {
             var time = Float(CACurrentMediaTime() - startTime)
             encoder.setFragmentBytes(&time, length: MemoryLayout<Float>.size, index: 0)
             
-            let dominantColor = AudioManager.shared.nowPlayingInfo.dominantColor.usingColorSpace(.deviceRGB) ?? .white
+            let dominant    
+Color = AudioManager.shared.nowPlayingInfo.dominantColor.usingColorSpace(.deviceRGB) ?? .white
             /// Generate A Tint From The Dominant Color
             var tint = SIMD3<Float>(
                 Float(dominantColor.redComponent),
